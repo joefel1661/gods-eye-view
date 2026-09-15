85bf45f42505e783de2a66e1c216220ba03b9410
joefel1661 <joefel1661@gmail.com>
2026-09-15 17:34:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import { createServer } from 'vite';
 
-const server = await createServer({
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 4173,
-  },
+async function start() {
+  const server = await createServer({
+    server: {
+      host: '0.0.0.0',
+      port: Number(process.env.PORT) || 4173,
+    },
+  });
+
+  await server.listen();
+  server.printUrls();
+}
+
+start().catch((error) => {
+  console.error(error);
+  process.exit(1);
 });
-
-await server.listen();
-
-server.printUrls();
